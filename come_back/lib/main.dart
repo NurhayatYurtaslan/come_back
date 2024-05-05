a7208a9949fdc1d5b5b2c943810cb6853bbb5de2
NurhayatYurtaslan <80510115+NurhayatYurtaslan@users.noreply.github.com>
2024-05-06 00:03:08 +0300

    update 🆕: main file

--- come_back/lib/main.dart
+++ come_back/lib/main.dart
@@ -1,6 +1,7 @@
-import 'package:come_back/product.dart';
+import 'package:come_back/view_product/product.dart';
+import 'package:come_back/repository/repositories.dart';
 import 'package:flutter/material.dart';
-
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 void main() {
   runApp(MyApp());
@@ -11,12 +12,15 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      
+      debugShowCheckedModeBanner: false,
       title: 'Your App Name',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const ProductView(),
+      home: RepositoryProvider(
+        create: (context) => ProductRepository(),
+        child: const ProductView(),
+      ),
     );
   }
 }
